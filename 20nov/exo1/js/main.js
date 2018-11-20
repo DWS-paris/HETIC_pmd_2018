410ef37b91f66c59bfa8d65252fa82a99d4f1e69
DWS-paris <j.noyer@digitalworkshop.fr>
2018-11-20 16:12:58 +0100

    Ajout des messages d'erreurs

--- 20nov/exo1/js/main.js
+++ 20nov/exo1/js/main.js
@@ -64,7 +64,12 @@ Création d'une fonction fetch
 Gestion du formulaire
 */
     const submitForm = () => {
+        // Déclaration des variables
         let myForm = document.querySelector('form');
+        let msgSubject = document.querySelector('[placeholder="Sujet"]');
+        let msgEmail = document.querySelector('[placeholder="Email"]');
+        let msgMessage = document.querySelector('[placeholder="Votre message"]');
+
         // Capter le submit du formulaire
         myForm.addEventListener( 'submit', (event) => {
             // Initier une variable pour la gestion des erreurs
@@ -73,27 +78,31 @@ Gestion du formulaire
             // Bloquer le comportement naturel de la balise
             event.preventDefault();
 
-            // Validation du formulaire
-            let msgSubject = document.querySelector('[placeholder="Sujet"]');
-            let msgEmail = document.querySelector('[placeholder="Email"]');
-            let msgMessage = document.querySelector('[placeholder="Votre message"]');
-
             // Le sujet est valide SI il contient au minimum 2 carctères
             if( msgSubject.value.length < 2){
                 // Incrémenter formError de 1
                 formError++
+
+                // Ajouter la class formError sur msgSubject
+                msgSubject.classList.add('formError')
             }
 
             // L'email est valide SI il contient au minimum 5 carctères
             if( msgEmail.value.length < 5){
                 // Incrémenter formError de 1
                 formError++
+
+                // Ajouter la class formError sur msgEmail
+                msgEmail.classList.add('formError')
             }
 
             // Le message est valide SI il contient au minimum 5 carctères
             if( msgMessage.value.length < 5){
                 // Incrémenter formError de 1
                 formError++
+
+                // Ajouter la class formError sur msgMessage
+                msgMessage.classList.add('formError')
             }
 
             // Validation finale du formulaire
@@ -103,6 +112,17 @@ Gestion du formulaire
                 // Envoyer les données...
                 // Vider le formulaire
             }
+        })
+
+        // Supprimer les messages d'erreur au focus
+        msgSubject.addEventListener( 'focus', () => {
+            msgSubject.classList.remove('formError')
+        })
+        msgEmail.addEventListener( 'focus', () => {
+            msgEmail.classList.remove('formError')
+        })
+        msgMessage.addEventListener( 'focus', () => {
+            msgMessage.classList.remove('formError')
         })
     };
 //
