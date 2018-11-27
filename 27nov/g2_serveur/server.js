157b350cf8f43c88b2d19db18ec95c033eda0ed0
DWS-paris <j.noyer@digitalworkshop.fr>
2018-11-27 12:34:11 +0100

    Server node

---
+++ 27nov/g2_serveur/server.js
@@ -0,0 +1,53 @@
+/* 
+Imports & declaration
+*/
+    // Importer express pour configurer le serveur
+    const express = require('express');
+
+    // Importer ejs pour le moteur de rendu
+    const ejs = require('ejs');
+
+    // Importer pth pour gérer le dossier des vues client
+    const path = require('path');
+
+    // Définir le port
+    const port = 9876;
+
+    // Créer une variable pour le serveur
+    const server = express();
+//
+
+/* 
+Server config
+*/
+    class ServerClass {
+
+        // Créer une méthode pour initialiser le serveur
+        init(){
+            // Configurer le moteur de rendu (engine)
+            server.engine( 'html', ejs.renderFile );
+            server.set( 'view engine', 'html' );
+
+            // Configurer le dossier pour les vues client
+            server.set( 'views', __dirname + '/www' );
+            server.use( express.static( path.join(__dirname, '/www') ) );
+
+            // Lancer le serveur
+            this.launch()
+        }
+
+        // Méthode pour lancer le serveur
+        launch(){
+            // Ecouter le port pour lancer le serveur
+            server.listen( port, () => {
+                console.log( `Le serveur est lancé sur le port ${port}` );
+            });
+        }
+    }
+//
+
+/* 
+Start server
+*/
+    new ServerClass().init();
+//
