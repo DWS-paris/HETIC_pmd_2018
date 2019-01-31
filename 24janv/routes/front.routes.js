cf2fc2c5d637ebba5c098a3b5ea2c54e46833a71
DWS-paris <j.noyer@digitalworkshop.fr>
2019-01-31 10:28:56 +0100

    Découpage EJS

--- 24janv/routes/front.routes.js
+++ 24janv/routes/front.routes.js
@@ -10,8 +10,27 @@ Définition
 */
     //=> Homepage
     frontRouter.get('/', ( req, res ) => {
-        //=> Envoyer le fichier "index" dans la réponse
-        res.render('index');
+        //=> Créeer une collection de todoes
+        const todoes = [
+            {
+                _id: 0,
+                content: "Créer un serveur Nodejs",
+                isDone: true
+            },
+            {
+                _id: 1,
+                content: "Ajouter des routes au serveur",
+                isDone: true
+            },
+            {
+                _id: 2,
+                content: "Faire une requête API depuis le client",
+                isDone: true
+            }
+        ]
+
+        //=> Envoyer le fichier "index" dans la réponse avec des donnnées
+        res.render('index', { data: todoes });
     });
 
     //=> Todo page
